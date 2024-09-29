7f1759af77dae1d75413efd65c7f9e4e8f4ab20f
MichaelDickinson20I <michael.dickinson3949@gmail.com>
2024-09-28 19:54:39 -0700

    handle saving telemetry data to db

---
+++ src/drone/serializers.py
@@ -0,0 +1,20 @@
+from drone.models import DroneTelemetry
+from rest_framework import serializers
+
+
+class DroneTelemetrySerializer (serializers.ModelSerializer):
+    vertical_velocity = serializers.FloatField(source="vertical_speed")
+    velocity = serializers.FloatField(source="speed")
+
+    class Meta:
+        model = DroneTelemetry
+        fields = (
+            "timestamp",
+            "latitude",
+            "longitude",
+            "altitude",
+            "vertical_velocity",
+            "velocity",
+            "heading",
+            "battery_voltage",
+        )
--- src/websocket/sockets.py
+++ src/websocket/sockets.py
@@ -1,11 +1,13 @@
-from typing import List
+from datetime import datetime, timedelta, timezone
+from typing import Any, List, Mapping
+from asgiref.sync import sync_to_async
 import socketio
 
 sio = socketio.AsyncServer(async_mode="asgi", cors_allowed_origins="*")
 
 
 @sio.event
-async def connect(sid: str) -> None:
+async def connect(sid: str, _: Mapping[str, Any]) -> None:
     """Runs whenever a client connects to the server
 
     Args:
@@ -34,3 +36,45 @@ async def log_message(sid: str, *args: List[str]) -> None:
         *args (List[str]): The arguments of the message
     """
     print(f"[SOCKET] Message from {sid}: {args}")
+
+
+@sio.on('ping')
+async def ping(sid: str) -> None:
+    """Runs whenever a client pings the server and emits a 'pong' event in response
+
+    Args:
+        sid (str): The session ID of the client pinging the server
+    """
+    print(f"[SOCKET] Ping from {sid}")
+    await sio.emit("pong", room=sid)
+
+
+@sio.on('drone_update')
+async def drone_update(sid: str, data: dict) -> None:
+    """Runs whenever the drone telemetry data is received
+    Saves the telemetry data to the database and deletes records over 5 mins old
+
+    Args:
+        sid (str): The session ID of the client sending the update
+        data (dict): The telemetry data of the drone
+    """
+
+    print(f"[SOCKET] Drone Update from {sid}: {data}")
+
+    await sync_to_async(process_drone_update)(data)
+
+
+def process_drone_update(data: dict) -> None:
+    """Handles the synchronous processing of the drone update."""
+    from drone.models import DroneTelemetry
+    from drone.serializers import DroneTelemetrySerializer
+
+    telemetry = DroneTelemetrySerializer(data=data)
+
+    if telemetry.is_valid():
+        telemetry.save()
+
+        cutoff_time = (datetime.now() - timedelta(minutes=5)).timestamp()
+        old_records = DroneTelemetry.objects.filter(
+            timestamp__lt=int(cutoff_time))
+        old_records.delete()
