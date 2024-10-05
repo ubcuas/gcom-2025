64f65df1749bbb25397aac30ed5e0489c788752a
21chanas3 <40689649+21chanas3@users.noreply.github.com>
2024-10-04 20:46:41 -0700

    test: Add model tests for Waypoint model.

--- src/nav/tests.py
+++ src/nav/tests.py
@@ -1,3 +1,54 @@
-# from django.test import TestCase
+from django.test import TestCase
+from .models import Waypoint
 
-# Create your tests here.
+class WaypointModelTest(TestCase):
+    def setUp(self):
+        Waypoint.objects.create(
+            name="Test Waypoint",
+            latitude=0,
+            longitude=0,
+            altitude=0
+        )
+
+    def test_waypoint(self):
+        waypoint = Waypoint.objects.get(name="Test Waypoint")
+        self.assertEqual(waypoint.name, "Test Waypoint")
+        self.assertEqual(waypoint.latitude, 0)
+        self.assertEqual(waypoint.longitude, 0)
+        self.assertEqual(waypoint.altitude, 0)
+
+    def test_cannot_create_without_name(self):
+        with self.assertRaises(Exception):
+            Waypoint.objects.create(
+                name=None,
+                latitude=0,
+                longitude=0,
+                altitude=0
+            )
+
+    def test_cannot_create_without_latitude(self):
+        with self.assertRaises(Exception):
+            Waypoint.objects.create(
+                name="Test Waypoint",
+                latitude=None,
+                longitude=0,
+                altitude=0
+            )
+
+    def test_cannot_create_without_longitude(self):
+        with self.assertRaises(Exception):
+            Waypoint.objects.create(
+                name="Test Waypoint",
+                latitude=0,
+                longitude=None,
+                altitude=0
+            )
+
+    def test_cannot_create_without_altitude(self):
+        with self.assertRaises(Exception):
+            Waypoint.objects.create(
+                name="Test Waypoint",
+                latitude=0,
+                longitude=0,
+                altitude=None
+            )
