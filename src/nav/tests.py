41aff3a3de4cde4b3558d0df534c98bb2dd31fa5
21chanas3 <40689649+21chanas3@users.noreply.github.com>
2024-10-04 20:54:42 -0700

    lint: black and ruff

--- src/nav/tests.py
+++ src/nav/tests.py
@@ -2,13 +2,11 @@ from django.test import TestCase
 from .models import Waypoint
 from rest_framework.test import APITestCase
 
+
 class WaypointModelTest(TestCase):
     def setUp(self):
         Waypoint.objects.create(
-            name="Test Waypoint",
-            latitude=0,
-            longitude=0,
-            altitude=0
+            name="Test Waypoint", latitude=0, longitude=0, altitude=0
         )
 
     def test_waypoint(self):
@@ -20,47 +18,31 @@ class WaypointModelTest(TestCase):
 
     def test_cannot_create_without_name(self):
         with self.assertRaises(Exception):
-            Waypoint.objects.create(
-                name=None,
-                latitude=0,
-                longitude=0,
-                altitude=0
-            )
+            Waypoint.objects.create(name=None, latitude=0, longitude=0, altitude=0)
 
     def test_cannot_create_without_latitude(self):
         with self.assertRaises(Exception):
             Waypoint.objects.create(
-                name="Test Waypoint",
-                latitude=None,
-                longitude=0,
-                altitude=0
+                name="Test Waypoint", latitude=None, longitude=0, altitude=0
             )
 
     def test_cannot_create_without_longitude(self):
         with self.assertRaises(Exception):
             Waypoint.objects.create(
-                name="Test Waypoint",
-                latitude=0,
-                longitude=None,
-                altitude=0
+                name="Test Waypoint", latitude=0, longitude=None, altitude=0
             )
 
     def test_cannot_create_without_altitude(self):
         with self.assertRaises(Exception):
             Waypoint.objects.create(
-                name="Test Waypoint",
-                latitude=0,
-                longitude=0,
-                altitude=None
+                name="Test Waypoint", latitude=0, longitude=0, altitude=None
             )
+
 
 class WaypointEndpointTests(APITestCase):
     def setUp(self):
         Waypoint.objects.create(
-            name="Test Waypoint",
-            latitude=0,
-            longitude=0,
-            altitude=0
+            name="Test Waypoint", latitude=0, longitude=0, altitude=0
         )
 
     def test_get_waypoint(self):
@@ -70,24 +52,22 @@ class WaypointEndpointTests(APITestCase):
         self.assertEqual(response.data[0]["name"], "Test Waypoint")
 
     def test_create_waypoint(self):
-        response = self.client.post("/api/waypoint/", {
-            "name": "Test Waypoint 2",
-            "latitude": 0,
-            "longitude": 0,
-            "altitude": 0
-        })
+        response = self.client.post(
+            "/api/waypoint/",
+            {"name": "Test Waypoint 2", "latitude": 0, "longitude": 0, "altitude": 0},
+        )
         self.assertEqual(response.status_code, 201)
         self.assertEqual(Waypoint.objects.count(), 2)
-        self.assertEqual(Waypoint.objects.get(name="Test Waypoint 2").name, "Test Waypoint 2")
+        self.assertEqual(
+            Waypoint.objects.get(name="Test Waypoint 2").name, "Test Waypoint 2"
+        )
 
     def test_update_waypoint(self):
         uuid = Waypoint.objects.get(name="Test Waypoint").id
-        response = self.client.put(f"/api/waypoint/{uuid}/", {
-            "name": "Updated Waypoint",
-            "latitude": 0,
-            "longitude": 0,
-            "altitude": 0
-        })
+        response = self.client.put(
+            f"/api/waypoint/{uuid}/",
+            {"name": "Updated Waypoint", "latitude": 0, "longitude": 0, "altitude": 0},
+        )
         self.assertEqual(response.status_code, 200)
         self.assertEqual(Waypoint.objects.get(id=uuid).name, "Updated Waypoint")
 
