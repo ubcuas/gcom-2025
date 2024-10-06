dd3f468c7c29f7a42bad9a6a318ce70fc49b23f2
MichaelDickinson20I <michael.dickinson3949@gmail.com>
2024-10-05 19:59:11 -0700

    added reordering action for waypoints

--- src/nav/views.py
+++ src/nav/views.py
@@ -1,6 +1,9 @@
 from rest_framework import viewsets
-from .models import Route, Waypoint
+from .models import Route, OrderedWaypoint
 from .serializers import RouteSerializer, OrderedWaypointSerializer
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework import status
 
 
 # Create your views here.
@@ -22,3 +25,41 @@ class RoutesViewset(viewsets.ModelViewSet):
 
     queryset = Route.objects.all()
     serializer_class = RouteSerializer
+
+    @action(detail=True, methods=["post"], url_path="reorder-waypoints")
+    def reorder_waypoints(self, request, pk=None):
+        """
+        Action to reorder waypoints in a route
+        Args:
+            request: The request object - contains the reordered waypoint ids
+            pk: The primary key of the route whose waypoints we're reordering
+        """
+        route = self.get_object()
+
+        waypoints = OrderedWaypoint.objects.filter(route=route)
+
+        if (
+            not request.data
+            or not isinstance(request.data, list)
+            or len(request.data) != len(waypoints)
+        ):
+            return Response(
+                {"error": "Please provide a list of all waypoint IDs"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        reordered_waypoint_ids = request.data
+        for idx, waypoint_id in enumerate(reordered_waypoint_ids):
+            try:
+                waypoint = waypoints.get(id=waypoint_id)
+                waypoint.order = idx
+                waypoint.save()
+            except OrderedWaypoint.DoesNotExist:
+                return Response(
+                    {"error": f"Waypoint {waypoint_id} not found"},
+                    status=status.HTTP_400_BAD_REQUEST,
+                )
+
+        return Response(
+            {"success": "Waypoints reordered successfully"}, status=status.HTTP_200_OK
+        )
