2f6026a1677cc4a1a9b81d2728f6047c911b7f86
xmy314 <kaishangxmy@gmail.com>
2024-10-05 23:43:01 -0700

    Test: Implemented test for mapping api
    Implemented for area of interest serializer and api as double book keeping for desired behaviour.

--- src/mapping/tests.py
+++ src/mapping/tests.py
@@ -1,3 +1,124 @@
-# from django.test import TestCase
+from django.test import TestCase
+from .views import post_area_of_interest
+from .views import AreaOfInterestSerializer
+from rest_framework.test import APITestCase
+from django.test.client import RequestFactory
+import json
 
-# Create your tests here.
+
+class AreaOfInterestValidationTest(TestCase):
+    def setUp(self):
+        pass
+
+    def test_expected(self):
+
+        # test for proper input
+        test_object = {
+            "area_of_interest": [
+                {"latitude": 1, "longitude": 1, "altitude": 1},
+                {"latitude": 1, "longitude": 1, "altitude": 1},
+                {"latitude": 1, "longitude": 1, "altitude": 1},
+                {"latitude": 1, "longitude": 1, "altitude": 1}
+            ]
+        }
+        valid_ser = AreaOfInterestSerializer(data=test_object)
+        self.assertEqual(valid_ser.is_valid(), True)
+
+        # test for missing a altitude
+        # in case boundary points don't include altitude
+        test_object = {
+            "area_of_interest": [
+                {"latitude": 1, "longitude": 1},
+                {"latitude": 1, "longitude": 1},
+                {"latitude": 1, "longitude": 1},
+                {"latitude": 1, "longitude": 1}
+            ]
+        }
+        valid_ser = AreaOfInterestSerializer(data=test_object)
+        if valid_ser.is_valid():
+            print(valid_ser.validated_data["area_of_interest"][0])
+        self.assertEqual(valid_ser.is_valid(), True)
+
+    def test_missing_attribute(self):
+
+        # test for missing area_of_interest
+        test_object = {
+        }
+        valid_ser = AreaOfInterestSerializer(data=test_object)
+        self.assertEqual(valid_ser.is_valid(), False)
+
+        # test for null area_of_interest
+        test_object = {
+            "area_of_interest": None
+        }
+        valid_ser = AreaOfInterestSerializer(data=test_object)
+        self.assertEqual(valid_ser.is_valid(), False)
+
+        # test for missing a latitude
+        test_object = {
+            "area_of_interest": [
+                {"longitude": 1, "altitude": 1},
+                {"latitude": 1, "longitude": 1, "altitude": 1},
+                {"latitude": 1, "longitude": 1, "altitude": 1},
+                {"latitude": 1, "longitude": 1, "altitude": 1}
+            ]
+        }
+        valid_ser = AreaOfInterestSerializer(data=test_object)
+        if valid_ser.is_valid():
+            print(valid_ser.validated_data["area_of_interest"][0])
+        self.assertEqual(valid_ser.is_valid(), False)
+
+        # test for null altitude
+        test_object = {
+            "area_of_interest": [
+                {"latitude": 1, "longitude": 1, "altitude": None},
+                {"latitude": 1, "longitude": 1, "altitude": 1},
+                {"latitude": 1, "longitude": 1, "altitude": 1},
+                {"latitude": 1, "longitude": 1, "altitude": 1}
+            ]
+        }
+        valid_ser = AreaOfInterestSerializer(data=test_object)
+        self.assertEqual(valid_ser.is_valid(), False)
+
+    def test_longer_list(self):
+        test_object = {
+            "area_of_interest": [
+                {"latitude": 1, "longitude": 1, "altitude": 1},
+                {"latitude": 1, "longitude": 1, "altitude": 1},
+                {"latitude": 1, "longitude": 1, "altitude": 1},
+                {"latitude": 1, "longitude": 1, "altitude": 1},
+                {"latitude": 1, "longitude": 1, "altitude": 1}
+            ]
+        }
+        valid_ser = AreaOfInterestSerializer(data=test_object)
+        self.assertEqual(valid_ser.is_valid(), False)
+
+    def test_shorter_list(self):
+        test_object = {
+            "area_of_interest": [
+                {"latitude": 1, "longitude": 1, "altitude": 1},
+                {"latitude": 1, "longitude": 1, "altitude": 1},
+                {"latitude": 1, "longitude": 1, "altitude": 1}
+            ]
+        }
+        valid_ser = AreaOfInterestSerializer(data=test_object)
+        self.assertEqual(valid_ser.is_valid(), False)
+
+
+class AreaOfInterestEndpointTest(APITestCase):
+    def setUp(self):
+        pass
+
+    def post_area_of_interest(self):
+        test_object = {
+            "area_of_interest": [
+                {"latitude": 1, "longitude": 1, "altitude": 1},
+                {"latitude": 1, "longitude": 1, "altitude": 1},
+                {"latitude": 1, "longitude": 1, "altitude": 1},
+                {"latitude": 1, "longitude": 1, "altitude": 1}
+            ]
+        }
+
+        response = self.client.get("/api/waypoint/area_of_interest", json.dumps(test_object))
+
+        self.assertEqual(response.status_code, 200)
