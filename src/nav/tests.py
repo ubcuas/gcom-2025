00a0f270aa4cbdc21f0a0035824eeb8e7d1eb2d0
MichaelDickinson20I <michael.dickinson3949@gmail.com>
2024-10-07 11:25:42 -0700

    updated waypoint tests and added route tests

--- src/nav/tests.py
+++ src/nav/tests.py
@@ -1,5 +1,6 @@
+import json
 from django.test import TestCase
-from .models import Waypoint
+from .models import OrderedWaypoint, Route, Waypoint
 from rest_framework.test import APITestCase
 
 
@@ -41,8 +42,16 @@ class WaypointModelTest(TestCase):
 
 class WaypointEndpointTests(APITestCase):
     def setUp(self):
-        Waypoint.objects.create(
-            name="Test Waypoint", latitude=0, longitude=0, altitude=0
+        test_route = Route.objects.create(name="Test Route")
+        self.test_route_id = test_route.id
+
+        OrderedWaypoint.objects.create(
+            name="Test Waypoint",
+            latitude=0,
+            longitude=0,
+            altitude=0,
+            order=0,
+            route=test_route,
         )
 
     def test_get_waypoint(self):
@@ -54,7 +63,14 @@ class WaypointEndpointTests(APITestCase):
     def test_create_waypoint(self):
         response = self.client.post(
             "/api/waypoint/",
-            {"name": "Test Waypoint 2", "latitude": 0, "longitude": 0, "altitude": 0},
+            {
+                "name": "Test Waypoint 2",
+                "latitude": 0,
+                "longitude": 0,
+                "altitude": 0,
+                "route": self.test_route_id,
+                "order": 1,
+            },
         )
         self.assertEqual(response.status_code, 201)
         self.assertEqual(Waypoint.objects.count(), 2)
@@ -63,10 +79,17 @@ class WaypointEndpointTests(APITestCase):
         )
 
     def test_update_waypoint(self):
-        uuid = Waypoint.objects.get(name="Test Waypoint").id
+        uuid = OrderedWaypoint.objects.get(name="Test Waypoint").id
         response = self.client.put(
             f"/api/waypoint/{uuid}/",
-            {"name": "Updated Waypoint", "latitude": 0, "longitude": 0, "altitude": 0},
+            {
+                "name": "Updated Waypoint",
+                "latitude": 0,
+                "longitude": 0,
+                "altitude": 0,
+                "route": self.test_route_id,
+                "order": 1,
+            },
         )
         self.assertEqual(response.status_code, 200)
         self.assertEqual(Waypoint.objects.get(id=uuid).name, "Updated Waypoint")
@@ -76,3 +99,90 @@ class WaypointEndpointTests(APITestCase):
         response = self.client.delete(f"/api/waypoint/{uuid}/")
         self.assertEqual(response.status_code, 204)
         self.assertEqual(Waypoint.objects.count(), 0)
+
+
+class RouteModelTest(TestCase):
+    def setUp(self):
+        Route.objects.create(name="Test Route")
+
+    def test_route(self):
+        route = Route.objects.get(name="Test Route")
+        self.assertEqual(route.name, "Test Route")
+
+    def test_cannot_create_without_name(self):
+        with self.assertRaises(Exception):
+            Route.objects.create(name=None)
+
+
+class RouteEndpointTests(APITestCase):
+    def setUp(self):
+        Route.objects.create(name="Test Route")
+
+    def test_get_route(self):
+        response = self.client.get("/api/route/")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.data), 1)
+        self.assertEqual(response.data[0]["name"], "Test Route")
+
+    def test_create_route(self):
+        response = self.client.post("/api/route/", {"name": "Test Route 2"})
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(Route.objects.count(), 2)
+        self.assertEqual(Route.objects.get(name="Test Route 2").name, "Test Route 2")
+
+    def test_update_route(self):
+        uuid = Route.objects.get(name="Test Route").id
+        response = self.client.put(f"/api/route/{uuid}/", {"name": "Updated Route"})
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(Route.objects.get(id=uuid).name, "Updated Route")
+
+    def test_delete_route(self):
+        uuid = Route.objects.get(name="Test Route").id
+        response = self.client.delete(f"/api/route/{uuid}/")
+        self.assertEqual(response.status_code, 204)
+        self.assertEqual(Route.objects.count(), 0)
+
+    def test_reorder_waypoints(self):
+        route = Route.objects.get(name="Test Route")
+        OrderedWaypoint.objects.create(
+            name="Test Waypoint 1",
+            latitude=0,
+            longitude=0,
+            altitude=0,
+            order=1,
+            route=route,
+        )
+        OrderedWaypoint.objects.create(
+            name="Test Waypoint 2",
+            latitude=0,
+            longitude=0,
+            altitude=0,
+            order=1,
+            route=route,
+        )
+
+        OrderedWaypoint.objects.create(
+            name="Test Waypoint 3",
+            latitude=0,
+            longitude=0,
+            altitude=0,
+            order=1,
+            route=route,
+        )
+
+        response = self.client.post(
+            f"/api/route/{route.id}/reorder-waypoints/",
+            content_type="application/json",
+            data=json.dumps(
+                [
+                    str(OrderedWaypoint.objects.get(name="Test Waypoint 2").id),
+                    str(OrderedWaypoint.objects.get(name="Test Waypoint 3").id),
+                    str(OrderedWaypoint.objects.get(name="Test Waypoint 1").id),
+                ]
+            ),
+        )
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(OrderedWaypoint.objects.get(name="Test Waypoint 2").order, 0)
+        self.assertEqual(OrderedWaypoint.objects.get(name="Test Waypoint 3").order, 1)
+        self.assertEqual(OrderedWaypoint.objects.get(name="Test Waypoint 1").order, 2)
