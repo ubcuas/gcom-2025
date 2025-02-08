fe1e349c1ad2a7cf0ca41153f630a55ba8bcbd24
MichaelDickinson20I <michael.dickinson3949@gmail.com>
2025-02-08 12:03:53 -0800

    added default route handling

--- src/nav/views.py
+++ src/nav/views.py
@@ -1,3 +1,4 @@
+from rest_framework.exceptions import NotFound
 from rest_framework import viewsets
 from .models import Route, OrderedWaypoint
 from .serializers import RouteSerializer, OrderedWaypointSerializer
@@ -25,6 +26,19 @@ class RoutesViewset(viewsets.ModelViewSet):
 
     queryset = Route.objects.all().prefetch_related("waypoints")
     serializer_class = RouteSerializer
+
+    def get_object(self):
+        pk = self.kwargs.get("pk")
+        try:
+            print(f"GETTING ROUTE {pk}")
+            return Route.objects.get(pk=pk)
+
+        except Route.DoesNotExist:
+            print(f"Checking if has pk 1")
+            print("TYPE E", type(pk))
+            if int(pk) == 1:
+                return Route.objects.create(id=1, name="Default Route")
+            raise NotFound(f"Route with id {pk} not found")
 
     @action(detail=True, methods=["post"], url_path="reorder-waypoints")
     def reorder_waypoints(self, request, pk=None):
